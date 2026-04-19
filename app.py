9a760c82f21324ff28a37fac8b8f2764a4cffa0f
Unathi Makhubela <unathi.makhubela7@gmail.com>
2026-04-19 23:57:15 +0200

    Initialized a loop for the two different modes

--- app.py
+++ app.py
@@ -5,6 +5,25 @@ import streamlit as st
 st.title('GhostWriter')
 st.subheader('The pen behind your sound')
 
+#Select a mode
+mode = st.radio(
+    "What do you need help with?",
+    ["Producing","Song Writing"],
+    captions=[
+        "Match the perfect beats.",
+        "Turn your ideas into life.",
+    ],
+    index=None,
+)
+
+if mode == "Producing":
+    st.subheader('Make the sounds in your head ideas')
+
+    st.tabs(["Chords","Progressions","Suggestions"])
+elif mode == "Song Writing":
+    st.subheader('The pen behind your sound')
+    st.tabs(["Lyrics","Brainstorm","Inspiration"])
+
 
 
 
