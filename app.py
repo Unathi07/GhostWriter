f2d08935c8f9aae5ba619a88b937d0adf6bf0d64
Unathi Makhubela <unathi.makhubela7@gmail.com>
2026-04-20 23:58:47 +0200

    Added chord selection

--- app.py
+++ app.py
@@ -29,6 +29,17 @@ if mode == "Producing":
 
     with tab1:
         st.header("Chords")
+        root_note=st.selectbox(
+            "Select a root note",
+            ("C","C#","D","D#","E","F","F#","G","G#","A","A#","B"),
+        )
+        st.write("You selected:", root_note)
+        chord_type=st.selectbox(
+            "Select a chord type",
+            ("Major","Minor","7th","Major 7th","Minor 7th","Suspended","Diminished"),
+        )
+        selected_chord = root_note + " " + chord_type
+        st.write("Chord:", selected_chord)
     with tab2:
         st.header("Progression")
     with tab3:
