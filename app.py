d304c89c425171d7324462fae1e43b8af3f95421
Unathi Makhubela <unathi.makhubela7@gmail.com>
2026-04-30 13:03:16 +0200

    Moved type_map outside the loop

--- app.py
+++ app.py
@@ -28,7 +28,21 @@ if mode == "Producing":
     #Tab options
     st.write("You selected:", options)
     tab1, tab2, tab3 = st.tabs(["Chords", "Progression", "Suggestions"])
-
+    #Chord name
+    type_map = {
+        "Major": "",
+        "Minor": "m",
+        "7th": "7",
+        "Major 7th": "maj7",
+        "Minor 7th": "min7",
+        "Suspended": "sus",
+        "Diminished": "dim",
+        "9th": "9",
+        "Major 9th": "maj9",
+        "Minor 9th": "min9",
+        "Add9": "add9",
+    }
+    #Information on the chords
     with tab1:
         st.header("Chords")
         root_note=st.selectbox(
@@ -40,19 +54,6 @@ if mode == "Producing":
             "Select a chord type",
             ("Major","Minor","7th","Major 7th","Minor 7th","Suspended","Diminished","9th","Major 9th","Minor 9th","Minor 9th","Add9"),
         )
-        type_map = {
-            "Major": "",
-            "Minor": "m",
-            "7th": "7",
-            "Major 7th": "maj7",
-            "Minor 7th": "min7",
-            "Suspended": "sus",
-            "Diminished": "dim",
-            "9th": "9",
-            "Major 9th": "maj9",
-            "Minor 9th": "min9",
-            "Add9": "add9",
-        }
         #Make chord for music21
         chord_map = type_map.get(chord_type)
         full_chord = root_note + chord_map
@@ -65,11 +66,31 @@ if mode == "Producing":
         st.write("Notes:", revised_notes)
 
         #HTML Code
-        render_piano()
-
-        st.components.v1.html(html, height=300)
+        st.components.v1.html(render_piano(revised_notes), height=300)
+    #Formulating a chord progression
     with tab2:
         st.header("Progression")
+        root_note2 = st.selectbox(
+            "Select a root note",
+            ("C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"),
+        )
+        st.write("You selected:", root_note2)
+        chord_type2 = st.selectbox(
+            "Select a chord type",
+            ("Major", "Minor", "7th", "Major 7th", "Minor 7th", "Suspended", "Diminished", "9th", "Major 9th",
+             "Minor 9th", "Minor 9th", "Add9"),
+        )
+        # Make chord for music21
+        chord_map2 = type_map.get(chord_type2)
+        full_chord2 = root_note2 + chord_map2
+        chord2 = harmony.ChordSymbol(full_chord2)
+        notes2 = [p.name for p in chord2.pitches]
+        revised_notes2 = [item.replace("-", "b") for item in notes2]
+
+        selected_chord2 = root_note2 + " " + chord_type2
+        st.write("Chord:", selected_chord2)
+        st.write("Notes:", revised_notes2)
+
     with tab3:
         st.header("Suggestions")
 
